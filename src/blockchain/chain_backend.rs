046555f2bb780afb62daeb43fe3775d362b5a975
Davidson Souza <iokwsg9ex@relay.firefox.com>
2022-12-21 13:20:57 -0300

    remove unnecessary module

--- src/blockchain/chain_backend.rs
+++
@@ -1,7 +0,0 @@
-//! In this codebase, we avoid sharing the logic for blockchain updates, like p2p messages
-//! and timers. The only shared struct is a [BlockchainState], a stateful RwLock protected
-//! struct representing our current view of the network. However, [BlockchainState] doesn't
-//! update itself, it relies an external "backend" that pulls blocks and transactions, applying
-//! to our current view.
-//! 
-//! This module defines the interface a object need to provide to update the chainstate.
