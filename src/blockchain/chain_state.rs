f33e0fb598d64e07f68c47bc2aa48a9d4cd6fc87
Davidson Souza <iokwsg9ex@relay.firefox.com>
2022-12-27 10:19:51 -0300

    remove unused static variable

--- src/blockchain/chain_state.rs
+++ src/blockchain/chain_state.rs
@@ -2,15 +2,13 @@ use std::{
     collections::{HashMap, HashSet},
     io::Write,
 };
-lazy_static::lazy_static!(
-    static ref REGTEST_GENESIS: Block = deserialize(&Vec::from_hex("0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4adae5494dffff7f20020000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000").unwrap()).unwrap();
-);
+
 use crate::{read_lock, write_lock};
 use async_std::{channel::Sender, task::block_on};
 use bitcoin::{
     blockdata::constants::genesis_block,
-    consensus::{deserialize, deserialize_partial, Encodable},
-    hashes::{hex::FromHex, sha256, Hash},
+    consensus::{deserialize_partial, Encodable},
+    hashes::{sha256, Hash},
     util::uint::Uint256,
     Block, BlockHash, BlockHeader, Network, OutPoint, Transaction, TxOut,
 };
