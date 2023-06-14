b044fedec45d7dd6a9cfe547dad969b5fe001314
Davidson Souza <iokwsg9ex@relay.firefox.com>
2023-06-14 14:05:23 -0300

    Move the node context to another file
    The node.rs file is over 1k lines, it would be better if we can reduce
    this a little. This commit moves all the node context data out of
    node.rs and puts the constants inside NodeContext.
    This is also useful, because we can fine-tune those constants by Context

---
+++ src/blockchain/p2p_blockchain/node_context.rs
@@ -0,0 +1,50 @@
+use std::{collections::HashMap, sync::Arc, time::Instant};
+
+use bitcoin::{network::utreexo::UtreexoBlock, BlockHash};
+
+use super::{node::RescanStatus, node_interface::NodeInterface};
+
+pub trait NodeContext {
+    const REQUEST_TIMEOUT: u64;
+    /// Max number of simultaneous connections we initiates we are willing to hold
+    const MAX_OUTGOING_PEERS: usize = 10;
+    /// We ask for peers every ASK_FOR_PEERS_INTERVAL seconds
+    const ASK_FOR_PEERS_INTERVAL: u64 = 60 * 60; // One hour
+    /// Save our database of peers every PEER_DB_DUMP_INTERVAL seconds
+    const PEER_DB_DUMP_INTERVAL: u64 = 60 * 5; // 5 minutes
+    /// Attempt to open a new connection (if needed) every TRY_NEW_CONNECTION seconds
+    const TRY_NEW_CONNECTION: u64 = 30; // 30 seconds
+    /// If ASSUME_STALE seconds passed since our last tip update, treat it as stale
+    const ASSUME_STALE: u64 = 30 * 60; // 30 minutes
+    /// While on IBD, if we've been without blocks for this long, ask for headers again
+    const IBD_REQUEST_BLOCKS_AGAIN: u64 = 10; // 10 seconds
+    /// How often we broadcast transactions
+    const BROADCAST_DELAY: u64 = 30; // 30 seconds
+    /// Max number of simultaneous inflight requests we allow
+    const MAX_INFLIGHT_REQUESTS: usize = 1_000;
+    /// Interval at which we open new feeler connections
+    const FEELER_INTERVAL: u64 = 60 * 5; // 5 minutes
+    /// Interval at which we rearrange our addresses
+    const ADDRESS_REARRANGE_INTERVAL: u64 = 60 * 60; // 1 hour
+    /// How long we ban a peer for
+    const BAN_TIME: u64 = 60 * 60 * 24;
+}
+
+#[derive(Debug, Clone)]
+pub struct RunningNode {
+    pub last_rescan_request: RescanStatus,
+    pub last_feeler: Instant,
+    pub last_address_rearrange: Instant,
+    pub user_requests: Arc<NodeInterface>,
+}
+impl NodeContext for RunningNode {
+    const REQUEST_TIMEOUT: u64 = 30;
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct IBDNode {
+    pub blocks: HashMap<BlockHash, UtreexoBlock>,
+}
+impl NodeContext for IBDNode {
+    const REQUEST_TIMEOUT: u64 = 30 * 60;
+}
