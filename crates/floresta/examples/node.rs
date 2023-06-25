9287a32cc00ed3613414e4cb75e46c0fc2315356
Davidson Souza <iokwsg9ex@relay.firefox.com>
2023-06-24 21:15:04 -0300

    Complete example

--- crates/floresta/examples/node.rs
+++ crates/floresta/examples/node.rs
@@ -84,6 +84,11 @@ async fn main() {
     }
 
     // Here we ask the node to grab the block with the given hash.
-    let block = handle.get_block(BlockHash::from_str("").unwrap()).unwrap();
+    let block = handle
+        .get_block(
+            BlockHash::from_str("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
+                .unwrap(),
+        )
+        .unwrap();
     println!("Block: {:?}", block);
 }
