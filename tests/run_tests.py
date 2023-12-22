8f41c44c1bb7000486abf11e6b278fa28bcafca4
Davidson Souza <iokwsg9ex@relay.firefox.com>
2023-12-22 15:32:48 -0300

    return error if some test dies

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -15,7 +15,7 @@ def main():
 
     if not os.path.isdir(data_dir):
         os.makedirs(data_dir)
-
+    failures = []
     for test_name in tests:
         test_dir = "./tests/" + test_name
         log_dir = data_dir + test_name.replace(".py", ".log")
@@ -25,9 +25,20 @@ def main():
         test.wait()
         if test.returncode != 0:
             print(f"Test {test_name} not passed {FAILURE_EMOJI}")
+            failures.append(test_name)
         else:
             print(f"Test {test_name} passed {SUCCESS_EMOJI}")
 
+    if len(failures) > 0:
+        print(f"{FAILURE_EMOJI} {len(failures)} tests failed")
+        for failure in failures:
+            # show logs for failed tests
+            log_dir = data_dir + failure.replace(".py", ".log")
+            print(f"Logs for {failure}:")
+            with open(log_dir, "rt") as log_file:
+                print(log_file.read())
+
+        raise Exception("Tests failed")
 
 if __name__ == '__main__':
     main()
