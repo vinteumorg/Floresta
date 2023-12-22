424df54655947fb48cb6b91aaa8517afc7fb87fd
Davidson Souza <iokwsg9ex@relay.firefox.com>
2023-12-22 14:57:14 -0300

    Minor ux improvements for run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,16 +4,20 @@ import time
 from tqdm import tqdm
 
 BASE_DIR = "/tmp/data"
+SUCCESS_EMOJI = "✔"
+FAILURE_EMOJI = "❌"
 
-tests = ["example_test", "restart"]
+tests = ["example_test", "restart", "test_getblockchaininfo", "test_getblockcount"]
 
 def main():
     print("Creating work dir")
     data_dir = BASE_DIR + f"run-{time.time()}/"
+    print(f'writing stuff to {data_dir}')
+
     if not os.path.isdir(data_dir):
         os.makedirs(data_dir)
-    print("Running tests")
-    for test_name in tqdm(tests):
+
+    for test_name in tests:
         test_dir = "./tests/" + test_name
         log_dir = data_dir + test_name.replace(".py", ".log")
         log_file = open(log_dir, "wt")
@@ -21,9 +25,9 @@ def main():
                                 stdout=log_file, stderr=log_file)
         test.wait()
         if test.returncode != 0:
-            print(f"Test {test_name} not passed")
+            print(f"Test {test_name} not passed {FAILURE_EMOJI}")
         else:
-            print(f"Test {test_name} passed")
+            print(f"Test {test_name} passed {SUCCESS_EMOJI}")
 
 
 if __name__ == '__main__':
