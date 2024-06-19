2431769603c73f2206b66b8a1a69c0c7f7e5b81d
jaoleal <jgleal@protonmail.com>
2024-06-19 11:41:44 -0300

    A flake and nix build expression for floresta.

---
+++ build.nix
@@ -0,0 +1,40 @@
+{ lib, rustPlatform, rust, buildInputs, nativeBuildInputs, ... }:
+
+let
+  pname = "florestad";
+  version = "0.5.1";
+
+  buildRustPackage = rustPlatform.buildRustPackage.override {
+    rustc = rust;
+    cargo = rust;
+  };
+in
+
+buildRustPackage {
+  inherit pname version;
+
+  doCheck = false;
+
+  src = ./.;
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "bitcoin-0.31.0" = "sha256-2v87cb+Wd4OhgDG45Za3bxumaY7QKdw7nKuqKrmtHMs=";
+    };
+  };
+
+  inherit buildInputs nativeBuildInputs;
+
+  # Override the Rust compiler used
+  rustc = "${rust}/bin/rustc";
+  cargo = "${rust}/bin/cargo";
+
+  meta = with lib; {
+    description = "A full bitcoin node with Utreexo";
+    homepage = "https://github.com/Davidson-Souza/Floresta";
+    license = licenses.agpl3;
+    maintainers = [ maintainers.Davidson maintainers.afm maintainers.jaoleal ];
+    platforms = platforms.all;
+  };
+}
