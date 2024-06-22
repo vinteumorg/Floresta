1d022d7d232b3e889e07e1a51fa14f237dd26611
lla-dane <abhinavagarwalla6@gmail.com>
2024-06-22 08:53:38 +0530

    merge conflict fixed

--- crates/floresta-wire/src/p2p_wire/chain_selector.rs
+++ crates/floresta-wire/src/p2p_wire/chain_selector.rs
@@ -623,7 +623,7 @@ where
                     self.1.sync_peer = *self.peer_ids.get(new_sync_peer).unwrap();
 
                     try_and_log!(
-                        self.request_headers(self.chain.get_best_block()?.1, new_sync_peer as u32)
+                        self.request_headers(self.chain.get_best_block()?.1, self.1.sync_peer)
                             .await
                     );
 
