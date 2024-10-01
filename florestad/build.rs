6a896259a6aeb81e49c4a5e2306542d027af5250
Davidson Souza <me@dlsouza.lol>
2024-10-01 12:29:48 -0300

    build.rs: make it work without a git enviroment
    If we try to build florestad outside the git tree, it will fail because
    the build script assumes we are inside one. But while building the
    Dockerfile and nix flake, we don't get the git files, so the build.rs
    breaks in those cases. This commit adds a fallback, where if we can't
    get the version using git, we use the one declared in our Cargo.toml.

--- florestad/build.rs
+++ florestad/build.rs
@@ -1,18 +1,5 @@
 fn main() {
-    let git_description = std::process::Command::new("git")
-        .args(["describe", "--tags", "--always", "--dirty"])
-        .output()
-        .map(|output| {
-            assert!(
-                output.status.success(),
-                "Failed to run git describe. Is git installed?"
-            );
-            let mut git_description = String::from_utf8(output.stdout).unwrap();
-            git_description.pop(); // remove the trailing newline
-            git_description
-        })
-        .expect("Failed to run git describe. Is git installed?");
-
+    let version = get_version_from_git().unwrap_or_else(|| get_version_from_manifest().unwrap());
     let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let runtime = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
@@ -29,12 +16,32 @@ fn main() {
         })
         .expect("Failed to run rustc --version. Is rustc installed?");
 
-    let version =
-        format!("version {git_description} compiled for {arch}-{os}-{runtime} with {rustc}");
-    println!("cargo:rustc-env=LONG_VERSION={}", version);
-    println!("cargo:rustc-env=GIT_DESCRIBE={}", git_description);
+    let long_version = format!("version {version} compiled for {arch}-{os}-{runtime} with {rustc}");
+    println!("cargo:rustc-env=LONG_VERSION={}", long_version);
+    println!("cargo:rustc-env=GIT_DESCRIBE={}", version);
 
     // re-run if either the build script or the git HEAD changes
     println!("cargo:rerun-if-changed=../.git/HEAD");
     println!("cargo:rerun-if-changed=build.rs");
+}
+
+fn get_version_from_manifest() -> Result<String, std::io::Error> {
+    let manifest = std::fs::read_to_string("Cargo.toml")?;
+    let toml: toml::Value = toml::from_str(&manifest).unwrap();
+    Ok(toml["package"]["version"].as_str().unwrap().to_string())
+}
+
+fn get_version_from_git() -> Option<String> {
+    std::process::Command::new("git")
+        .args(["describe", "--tags", "--always", "--dirty"])
+        .output()
+        .map(|output| {
+            if !output.status.success() {
+                return None;
+            }
+            let mut git_description = String::from_utf8(output.stdout).unwrap();
+            git_description.pop(); // remove the trailing newline
+            Some(git_description)
+        })
+        .ok()?
 }
