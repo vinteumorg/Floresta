2f5ccf8f33005cfc4b0214a4498103f7953b9f90
qlrd <qlrddev qlrddev@gmail.com>
2024-12-22 13:12:11 -0300

    linted tests/test_framework/test_framework.py, renamed the class as FlorestaTestFramework and refactored some code to follow some principles of BitcoinCore functional tests
    linted tests/example_test.py, added assertions and modified it to be in accord with the test_framework
    
    linted tests/restart.py and applied the main call at the end of file

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,31 +1,116 @@
 """
-    This is an example of how tests should look lke, see the class bellow for more info
+example_test.py
+
+This is an example of how tests should look like, see the class bellow for more info.
+
+Every test should define, at least, two special methods:
+
+- `set_test_params`: change default values for number of node, topology, etc.
+- `run_test`: the test itself
+
+Any attempt to define a test without these methods will raise a TypeError.
+
+Other methods are available to do more things. Some of them make sense only in `set_test_params`
+and others make sense only in `run_tests`. 
+
+- `add_node_settings`: register a node settings to create a node after. A node will be a spawned
+                       `cargo run --features json-rpc --bin florestad -- --network <chain>` process.
+                       In summary, its a `FlorestaRPC` instance.
+                       
+                       The chain can be one of ["regtest", "signet", "testnet"].
+                       You can pass some extra arguments with `extra_args` and it will be appended
+                       to the process command. The `rpcserver` is a dictionary defining a "host",
+                       "port", "username" and "password". The "data_dir" is optional and can be
+                       used to create a temporary directory to store files. It will return an
+                       integer pointing an index of a list of nodes.
+                       
+- `get_node_settings`: get a registered node settings
+
+- `run_node`: run a node for a registered node settings at some index,
+              configured with `add_node_settings`.
+
+- `get_node`: get a resgistered running node.
+
+- `wait_for_rpc_connection`: given a node index, wait for it to be available.
+
+- `run_rpc`: our RPC is a MockUtreexod running on a thread.
+
+- `stop_node`: given a running node at index, stop it.
+               If any directory was created, it will be removed.
+
+- `stop`: stop all registered nodes.
+
+After the definition of test within a class, you should call `MyTest().main()` at the end of file.
 """
-import time
-import os
 
-from test_framework.test_framework import TestFramework
+import json
+from test_framework.test_framework import FlorestaTestFramework
 from test_framework.electrum_client import ElectrumClient
-from test_framework.mock_rpc import MockUtreexod
+from test_framework.floresta_rpc import REGTEST_RPC_SERVER
 
 
-class ExampleTest(TestFramework):
-    """ Tests should be a child class from TestFramework """
+class ExampleTest(FlorestaTestFramework):
+    """
+    Tests should be a child class from FlorestaTestFramework
+
+    In each test class definition, `set_test_params` and `run_test`, say what
+    the test do and the expected result in the docstrings
+    """
+
+    index = [-1]
+    expected_version = ["Floresta 0.3.0", "1.4"]
+    expected_height = 0
+    expected_block = "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"
+    expected_difficulty = 1
+    expected_leaf_count = 0
+
+    def set_test_params(self):
+        """
+        Here we define setup for test adding a node definition
+        """
+        ExampleTest.index[0] = self.add_node_settings(
+            chain="regtest", extra_args=[], rpcserver=REGTEST_RPC_SERVER
+        )
 
     # All tests should override the run_test method
     def run_test(self):
+        """
+        Here we define the test itself:
+
+        - creates a dummy rpc listening on port 8080
+        - wait until the rpc is ready and start a new node (this crate's binary)
+        - wait the node to start
+        - perform some requests to FlorestaRPC node
+        - Create an instance of the Electrum Client, a small implementation of the electrum
+          protocol, to test our own electrum implementation
+        """
         # This creates a dummy rpc listening on port 8080
         self.run_rpc()
+
         # Wait until the rpc is ready
         # Start a new node (this crate's binary)
-        node1 = self.run_node("./data/test1", "regtest")
+        self.run_node(ExampleTest.index[0])
+
         # Wait the node to start
-        self.wait_for_rpc_connection()
+        self.wait_for_rpc_connection(ExampleTest.index[0])
+
+        # Perform for some defined requests to FlorestaRPC
+        node = self.get_node(ExampleTest.index[0])
+        inf_response = node.get_blockchain_info()
+
         # Create an instance of the Electrum Client, a small implementation of the electrum
         # protocol, to test our own electrum implementation
         electrum = ElectrumClient("localhost", 50001)
-        print(electrum.get_version())
+        rpc_response = json.loads(electrum.get_version())
+
+        # Make assertions
+        assert rpc_response["result"][0] == ExampleTest.expected_version[0]
+        assert rpc_response["result"][1] == ExampleTest.expected_version[1]
+        assert inf_response["height"] == ExampleTest.expected_height
+        assert inf_response["best_block"] == ExampleTest.expected_block
+        assert inf_response["difficulty"] == ExampleTest.expected_difficulty
+        assert inf_response["leaf_count"] == ExampleTest.expected_leaf_count
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ExampleTest().main()
--- tests/restart.py
+++ tests/restart.py
@@ -1,21 +1,65 @@
-import subprocess
+"""
+restart.py
+
+A simple test that restart a Floresta node and a related data directory.
+
+The directories used between each power-on/power-off must not be corrupted.
+"""
+
 import time
 import os
 import filecmp
-from test_framework.test_framework import TestFramework
+from test_framework.test_framework import FlorestaTestFramework
+from test_framework.floresta_rpc import REGTEST_RPC_SERVER
 
 
-class TestRestart(TestFramework):
+class TestRestart(FlorestaTestFramework):
+    """
+    Test the restart of a node, calling a first node (0) the recall it as (1);
+    We need to check if given data_dirs arent corrupted between restarts
+    """
+
+    indexes = [-1, -1]
+
+    def set_test_params(self):
+        """
+        Here we define setup for test
+        """
+        dirname = os.path.dirname(__file__)
+        TestRestart.indexes[0] = self.add_node_settings(
+            chain="regtest",
+            extra_args=[],
+            rpcserver=REGTEST_RPC_SERVER,
+            data_dir=os.path.normpath(os.path.join(dirname, "data", "0")),
+        )
+        TestRestart.indexes[1] = self.add_node_settings(
+            chain="regtest",
+            extra_args=[],
+            rpcserver=REGTEST_RPC_SERVER,
+            data_dir=os.path.normpath(os.path.join(dirname, "data", "1")),
+        )
+
     def run_test(self):
         """
-            Tests if we don't corrupt our data dir between restarts. This would have caught,
-            the error fixed in #9
+        Tests if we don't corrupt our data dir between restarts. This would have caught,
+        the error fixed in #9
         """
-        base_testdir = "data/TestRestart/"
-        self.run_node(base_testdir + "1/")
-        time.sleep(5)
-        self.stop_node(0)
-        self.run_node(base_testdir + "2/")
-        time.sleep(5)
-        self.stop_node(0)
-        assert (filecmp.dircmp(base_testdir + "2/", base_testdir + "1/"))
+        # start first node, wait and then kill
+        self.run_node(TestRestart.indexes[0])
+        time.sleep(5.0)
+        self.stop_node(TestRestart.indexes[0])
+
+        # start second node, wait and then kill
+        self.run_node(TestRestart.indexes[1])
+        time.sleep(5.0)
+        self.stop_node(TestRestart.indexes[1])
+
+        # check for any corruption
+        assert filecmp.dircmp(
+            self.get_node_settings(TestRestart.indexes[0])["data_dir"],
+            self.get_node_settings(TestRestart.indexes[1])["data_dir"],
+        )
+
+
+if __name__ == "__main__":
+    TestRestart().main()
