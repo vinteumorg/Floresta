e1d5ecba4ceb1237da041ee49b3ecfa6b9df2b39
Shigeru Myamoto <jgleal@protonmail.com>
2025-01-07 13:56:03 -0300

    Bump back nix building to 23.11 and remove bitcoin fork (#334)

--- build.nix
+++ build.nix
@@ -1,17 +1,17 @@
 { lib, rustPlatform, florestaRust, buildInputs }:
 
 let
-    # Pname defines the name of the package and will decide the output of this expression
-    pname = "floresta-node";
-    version = "0.7.0";
+  # Pname defines the name of the package and will decide the output of this expression
+  pname = "floresta-node";
+  version = "0.7.0";
 
-    # This sets the rustc and cargo to the ones from the florestaRust.
-    #
-    # Defined in Flake.nix directly from the rust-toolchain.
-    buildRustPackage = rustPlatform.buildRustPackage.override {
-        rustc = florestaRust;
-        cargo = florestaRust;
-    };
+  # This sets the rustc and cargo to the ones from the florestaRust.
+  #
+  # Defined in Flake.nix directly from the rust-toolchain.
+  buildRustPackage = rustPlatform.buildRustPackage.override {
+    rustc = florestaRust;
+    cargo = florestaRust;
+  };
 in
 buildRustPackage {
   inherit pname version;
@@ -22,9 +22,6 @@ buildRustPackage {
 
   cargoLock = {
     lockFile = ./Cargo.lock;
-    outputHashes = {
-      "bitcoin-0.31.0" = "sha256-2v87cb+Wd4OhgDG45Za3bxumaY7QKdw7nKuqKrmtHMs=";
-    };
   };
 
   inherit buildInputs;
