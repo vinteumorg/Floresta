492b016732984711153ce07ee221582c23e9d0db
Luis Schwab <97608688+luisschwab@users.noreply.github.com>
2025-01-21 14:42:26 -0300

    chore: add new utreexo seed [seed.bitcoin.luisschwab.com] (#343)

--- crates/floresta-chain/src/pruned_utreexo/chainparams.rs
+++ crates/floresta-chain/src/pruned_utreexo/chainparams.rs
@@ -277,6 +277,11 @@ pub fn get_chain_dns_seeds(network: Network) -> Vec<DnsSeed> {
         Network::Bitcoin => {
             seeds.push(DnsSeed::new(
                 Network::Bitcoin,
+                "seed.bitcoin.luisschwab.com",
+                x1000049,
+            ));
+            seeds.push(DnsSeed::new(
+                Network::Bitcoin,
                 "seed.bitcoin.sipa.be",
                 x9, // no COMPACT_FILTERS
             ));
