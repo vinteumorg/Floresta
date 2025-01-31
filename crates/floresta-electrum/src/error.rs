70d1ae04c403134e9a4e0ed4b833bf9edb58bea4
Siddhesh Z <siddheshzope1023@gmail.com>
2025-01-31 22:46:55 +0530

    Remove unused cfg attributes

--- crates/floresta-electrum/src/error.rs
+++ crates/floresta-electrum/src/error.rs
@@ -1,12 +1,7 @@
-#[cfg(feature = "cli-blockchain")]
-use btcd_rpc::error::UtreexodError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Utreexod error")]
-    #[cfg(feature = "cli-blockchain")]
-    BackendError(#[from] UtreexodError),
     #[error("Invalid params passed in")]
     InvalidParams,
     #[error("Invalid json string {0}")]
