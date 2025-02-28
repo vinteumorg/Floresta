04eec9589484ac525cb5d9f5d85a93e45533e2fc
jaoleal <jgleal@protonmail.com>
2025-02-28 18:20:47 -0300

    Fixed: python tests nix-shell * nix-shell: Added variation that doesnt run the tests. * nix-shell: Explicit use of same version of rust than rust-toolchain. * nix-shell: Added poe. * prepare.sh now executes "poetry install --no-root" to set tests dependencies. * floresta_cli_getrpcinfo.py now is path agnostic without its assertion on path tree.

--- flake.nix
+++ flake.nix
@@ -77,36 +77,36 @@
         devShells = {
           pythonTests =
             let
-              _scriptSetup = ''
-                mkdir -p ./bin
-
-                cd bin
-
-                # Download and build utreexod
-                ls -la utreexod &>/dev/null
-
-                if [ $? -ne 0 ]
-                then
-                  	git clone https://github.com/utreexo/utreexod
-                fi
-                cd utreexod
-
-                go build . &>/dev/null
-                echo "All done!"
-              '';
-              _scriptRun = "poetry run poe tests";
+              _scriptSetup = ''bash ./tests/prepare.sh'';
             in
             pkgs.mkShell {
               buildInputs = with pkgs; [
-                cargo
+                florestaRust
                 python312
                 poetry
+                poethepoet
                 go
-              ] ++ [ self.packages.${system}.default ];
+              ];
               shellHook = ''
                 ${_scriptSetup}
-                ${_scriptRun}
-
+                echo -e "you may execute \n\tpoetry run poe tests \nto execute Florestas Python tests"
+              '';
+            };
+          runPythonTests =
+            let
+              _scriptSetup = ''bash ./tests/prepare.sh'';
+            in
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                florestaRust
+                python312
+                poetry
+                poethepoet
+                go
+              ];
+              shellHook = ''
+                ${_scriptSetup}
+                poetry run poe tests
                 exit
               '';
             };
