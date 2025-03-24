b2a6171c9126cad8013878cb39643e27ca23b1a5
qlrd <106913782+qlrd@users.noreply.github.com>
2025-03-24 13:25:45 +0000

    Added functional tests for SSL/TLS (#422)
    fix #421
    
    * pyproject.toml/uv.lock: added cryptography lib to enable the creation
    of private keys and self-signed certificates;
    
    * created tests/test_framework/crypto/pkcs8.py: this module generate
    well formatted pkcs8 private keys and self-signed certificates for our
    SSL-enable florestad node on regtest network;
    
    * tests/florestad/ssl-test.py: test the creation of a SSL-enabled
    florestad node, a electrum client that try to connect to port 50002 and
    request a ping, asserting a successfully connection;
    
    * tests/florestad/ssl-fail-test.py: test the creation of a SSL-disabled
    florestad node, a electrum client that try to connect to port 50002,
    asserting a failure on connection.

---
+++ tests/florestad/ssl-fail-test.py
@@ -0,0 +1,52 @@
+"""
+florestad/ssl-fail-test.py
+
+This functional test checks the failure on connect to florestad's TLS port.
+"""
+
+import errno
+
+from test_framework.electrum_client import ElectrumClient
+from test_framework.floresta_rpc import REGTEST_RPC_SERVER
+from test_framework.test_framework import FlorestaTestFramework
+
+
+class TestSslFailInitialization(FlorestaTestFramework):
+    """
+    Test the initialization of florestad without --ssl-key-path and --ssl-cert-path
+    (and without proper keys), a request from Electrum client to TLS port and its failure.
+    """
+
+    nodes = [-1]
+    electrum = None
+
+    def set_test_params(self):
+        """
+        Setup a single node without SSL
+        """
+        TestSslFailInitialization.nodes[0] = self.add_node_settings(
+            chain="regtest", extra_args=[], rpcserver=REGTEST_RPC_SERVER, ssl=False
+        )
+
+    def run_test(self):
+        """
+        Run the no-ssl node, create an electrum client that will try to connect to port 50002,
+        and assert a connection refused failure.
+        """
+        self.run_node(TestSslFailInitialization.nodes[0])
+        self.wait_for_rpc_connection(TestSslFailInitialization.nodes[0])
+
+        # now try create a connection with an electrum client at default port
+        # it must fail, since the TLS port isnt opened
+        try:
+            TestSslFailInitialization.electrum = ElectrumClient("0.0.0.0", 50002)
+
+        except ConnectionRefusedError as exc:
+            assert exc.errno == errno.ECONNREFUSED
+
+        # Shutdown node
+        self.stop_node(TestSslFailInitialization.nodes[0])
+
+
+if __name__ == "__main__":
+    TestSslFailInitialization().main()
