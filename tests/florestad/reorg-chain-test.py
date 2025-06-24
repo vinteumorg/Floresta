23a91f1b44fabfb54d8321378f69e66c3a0a78d0
qlrddev <qlrddev@gmail.com>
2025-06-24 14:18:27 -0300

    Renamed some tests to comply with changes
    * renamed reorg-chain.py to reorg-chain-test.py;
    * renamed utreexo-test.py to utreexod-test.py.

--- tests/florestad/reorg-chain-test.py
+++ tests/florestad/reorg-chain-test.py
@@ -7,14 +7,18 @@ make florestad switch to the new chain. We then compare the two node's main chai
 accumulator to make sure they are the same.
 """
 
+from os.path import commonpath
+import re
+import time
+
 from test_framework import FlorestaRPC, UtreexoRPC, FlorestaTestFramework
 from test_framework.rpc.floresta import REGTEST_RPC_SERVER as florestad_rpc
 from test_framework.rpc.utreexo import REGTEST_RPC_SERVER as utreexod_rpc
 
-import time
-
 
 class ChainReorgTest(FlorestaTestFramework):
+    """Test the reorganization of the chain in florestad when using utreexod to mine blocks."""
+
     index = [-1, -1]
     expected_chain = "regtest"
 
@@ -23,17 +27,10 @@ class ChainReorgTest(FlorestaTestFramework):
             variant="florestad", rpcserver=florestad_rpc
         )
 
-        # since utreexod and bitcoind
-        # uses the same RPC server, we need to
-        # select a different port for utreexod
-        utreexod_rpc["ports"]["server"] = 18446
-        utreexod_rpc["ports"]["rpc"] = 18447
         ChainReorgTest.index[1] = self.add_node(
             variant="utreexod",
             rpcserver=utreexod_rpc,
             extra_args=[
-                "--listen=127.0.0.1:18446",
-                "--rpclisten=127.0.0.1:18447",
                 "--miningaddr=bcrt1q4gfcga7jfjmm02zpvrh4ttc5k7lmnq2re52z2y",
                 "--utreexoproofindex",
                 "--prune=0",
@@ -45,43 +42,52 @@ class ChainReorgTest(FlorestaTestFramework):
         self.run_node(ChainReorgTest.index[0])
         self.run_node(ChainReorgTest.index[1])
 
+        florestad: FlorestaRPC = self.get_node(ChainReorgTest.index[0]).rpc
         utreexod: UtreexoRPC = self.get_node(ChainReorgTest.index[1]).rpc
 
         # Mine some blocks with utreexod
-        self.log("Mining blocks with utreexod...")
+        self.log("=== Mining blocks with utreexod")
         utreexod.generate(10)
 
-        # Connect floresta to utreexod
-        florestad: FlorestaRPC = self.get_node(ChainReorgTest.index[0]).rpc
-        florestad.addnode(
-            f"{utreexod_rpc["host"]}:{utreexod_rpc["ports"]["server"]}", "onetry"
+        self.log("=== Connect floresta to utreexod")
+        host = utreexod_rpc["host"]
+        port = utreexod_rpc["ports"]["p2p"]
+        florestad.addnode(f"{host}:{port}", command="onetry", v2transport=False)
+        time.sleep(5)
+
+        self.log("=== Waiting for floresta to connect to utreexod...")
+        peer_info = florestad.get_peerinfo()
+        self.assertMatch(
+            peer_info[0]["user_agent"],
+            re.compile(r"/btcwire:\d+\.\d+\.\d+/utreexod:\d+\.\d+\.\d+/"),
         )
 
+        self.log("=== Wait for the nodes to sync...")
         time.sleep(20)
 
-        # Check that floresta has the same chain as utreexod
+        self.log("=== Check that floresta has the same chain as utreexod...")
         floresta_chain = florestad.get_blockchain_info()
         utreexo_chain = utreexod.get_blockchain_info()
-
+        self.assertEqual(floresta_chain["best_block"], utreexo_chain["bestblockhash"])
         self.assertEqual(floresta_chain["height"], utreexo_chain["blocks"])
 
+        self.log("=== Get a block hash from utreexod to invalidate")
         hash = utreexod.get_blockhash(5)
-        self.log(f"Block hash at height 5: {hash}")
         utreexod.invalidate_block(hash)
 
-        # Mine an alternative chain with 20 blocks
-        self.log("Mining alternative chain with utreexod...")
-        utreexod.generate(20)
+        self.log("=== Mining alternative chain with utreexod...")
+        utreexod.generate(10)
 
-        # Wait for the nodes to sync
-        time.sleep(10)
+        self.log("=== Wait for the nodes to sync")
+        time.sleep(20)
 
-        # Check that floresta has switched to the new chain
+        self.log("=== Check that floresta has switched to the new chain")
         floresta_chain = florestad.get_blockchain_info()
         utreexo_chain = utreexod.get_blockchain_info()
+        self.assertEqual(floresta_chain["best_block"], utreexo_chain["bestblockhash"])
         self.assertEqual(floresta_chain["height"], utreexo_chain["blocks"])
 
-        # Compare the accumulator roots for each node
+        self.log("=== Compare the accumulator roots for each node")
         floresta_roots = florestad.get_roots()
         utreexo_roots = utreexod.get_utreexo_roots(utreexo_chain["bestblockhash"])
         self.assertEqual(floresta_roots, utreexo_roots["roots"])
