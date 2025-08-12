ea9e483d8ed95d43414628eac58823dbbaffc876
qlrd <106913782+qlrd@users.noreply.github.com>
2025-08-12 20:07:29 +0000

    test: improve `getblockhash` functional test (#605)
    * test: improve getblockhash functional test
    
    This commit add a `utreexod` and a `bitcoin-core` nodes to
    `getblockhash` test, mine 10 blocks with the `utreexod` node and compare
    the `getblockhash` outputs for each node with a provided height.
    
    Follow-up of #453.
    
    * feat(test): add `get_blockhash` method for bitcoin-core rpc

--- tests/floresta-cli/getblockhash.py
+++ tests/floresta-cli/getblockhash.py
@@ -1,15 +1,21 @@
 """
-floresta_cli_getblockhash.py
+getblockhash.py
 
 This functional test cli utility to interact with a Floresta node with `getblockhash`
 """
 
+import re
+import time
 from test_framework import FlorestaTestFramework
+
+DATA_DIR = FlorestaTestFramework.get_integration_test_dir()
 
 
 class GetBlockhashTest(FlorestaTestFramework):
     """
-    Test `getblock` with a fresh node and expected initial 0 height block
+    Test `getblockhash` with a fresh node and expected initial 0 height block.
+    After that, it will mine some blocks with utreexod and check that
+    the blockhashes match between floresta, utreexod, and bitcoind.
     """
 
     best_block = "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"
@@ -18,18 +24,79 @@ class GetBlockhashTest(FlorestaTestFramework):
         """
         Setup a single node
         """
-        self.florestad = self.add_node(variant="florestad")
+        name = self.__class__.__name__.lower()
+        self.v2transport = False
+        self.data_dirs = GetBlockhashTest.create_data_dirs(DATA_DIR, name, 3)
+
+        self.florestad = self.add_node(
+            variant="florestad", extra_args=[f"--data-dir={self.data_dirs[0]}"]
+        )
+
+        self.utreexod = self.add_node(
+            variant="utreexod",
+            extra_args=[
+                f"--datadir={self.data_dirs[1]}",
+                "--miningaddr=bcrt1q4gfcga7jfjmm02zpvrh4ttc5k7lmnq2re52z2y",
+                "--prune=0",
+            ],
+        )
+
+        self.bitcoind = self.add_node(
+            variant="bitcoind", extra_args=[f"-datadir={self.data_dirs[2]}"]
+        )
 
     def run_test(self):
         """
-        Run JSONRP and get the hash of height 0
+        Run JSONRPC and get the hash of heights 0 to 10.
         """
-        # Start node
+        self.log("=== Starting nodes...")
         self.run_node(self.florestad)
+        self.run_node(self.utreexod)
+        self.run_node(self.bitcoind)
 
-        # Test assertions
-        response = self.florestad.rpc.get_blockhash(0)
-        self.assertEqual(response, GetBlockhashTest.best_block)
+        self.log("=== Mining blocks with utreexod")
+        self.utreexod.rpc.generate(10)
+        time.sleep(5)
+
+        self.log("=== Connect floresta to utreexod")
+        host = self.utreexod.get_host()
+        port = self.utreexod.get_port("p2p")
+        self.florestad.rpc.addnode(
+            f"{host}:{port}", command="onetry", v2transport=False
+        )
+
+        self.log("=== Waiting for floresta to connect to utreexod...")
+        time.sleep(5)
+        peer_info = self.florestad.rpc.get_peerinfo()
+        self.assertMatch(
+            peer_info[0]["user_agent"],
+            re.compile(r"/btcwire:\d+\.\d+\.\d+/utreexod:\d+\.\d+\.\d+/"),
+        )
+
+        self.log("=== Connect bitcoind to utreexod")
+        self.bitcoind.rpc.addnode(f"{host}:{port}", command="onetry", v2transport=False)
+
+        self.log("=== Waiting for bitcoind to connect to utreexod...")
+        time.sleep(5)
+        peer_info = self.bitcoind.rpc.get_peerinfo()
+        self.assertMatch(
+            peer_info[0]["subver"],
+            re.compile(r"/btcwire:\d+\.\d+\.\d+/utreexod:\d+\.\d+\.\d+/"),
+        )
+
+        self.log("=== Wait for the nodes to sync...")
+        time.sleep(5)
+
+        self.log("=== Get the tip block")
+        block_count = self.florestad.rpc.get_block_count()
+
+        for i in range(0, block_count + 1):
+            self.log(f"=== Check the correct blockhash for height {i}...")
+            hash_floresta = self.florestad.rpc.get_blockhash(i)
+            hash_utreexod = self.utreexod.rpc.get_blockhash(i)
+            hash_bitcoind = self.bitcoind.rpc.get_blockhash(i)
+            for _hash in [hash_utreexod, hash_bitcoind]:
+                self.assertEqual(hash_floresta, _hash)
 
         # stop the node
         self.stop()
--- tests/test_framework/rpc/bitcoin.py
+++ tests/test_framework/rpc/bitcoin.py
@@ -98,3 +98,16 @@ class BitcoinRPC(BaseRPC):
             raise ValueError(f"Invalid command '{command}'")
 
         return self.perform_request("addnode", params=[node, command, v2transport])
+
+    def get_blockhash(self, height: int) -> str:
+        """
+        Get the block hash at a given height by performing
+        `perform_request('getblockhash', params=[int])`
+
+        Args:
+            height: The height of the block to get the hash for
+
+        Returns:
+            The block hash as a string
+        """
+        return self.perform_request("getblockhash", params=[height])
