b23cc5efff4e73d0eeca752b5582c793ccfb7e68
Davidson Souza <me@dlsouza.lol>
2025-08-14 13:04:40 -0300

    wire: allow sync node to make more block requests
    Since now we are not asking for blocks from the same peers, we can
    increase the amount of inflight requests we can sustain. I'm increasing
    the number of `getdata` requests to 10, and each one can have at most
    5 blocks, for a total of 50 blocks. At a 4MiB maximum, that's a 200MiB
    limit of data (actually a little more due to them being parsed, but still
    manageable).

--- crates/floresta-wire/src/p2p_wire/sync_node.rs
+++ crates/floresta-wire/src/p2p_wire/sync_node.rs
@@ -43,8 +43,7 @@ impl NodeContext for SyncNode {
         ServiceFlags::WITNESS | service_flags::UTREEXO.into() | ServiceFlags::NETWORK
     }
 
-    const MAX_OUTGOING_PEERS: usize = 5; // don't need many peers, half the default
-    const TRY_NEW_CONNECTION: u64 = 60; // one minute
+    const TRY_NEW_CONNECTION: u64 = 30; // 30 seconds
     const REQUEST_TIMEOUT: u64 = 10 * 60; // 10 minutes
     const MAX_INFLIGHT_REQUESTS: usize = 100; // double the default
 }
