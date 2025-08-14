ec14071b24615e8285f7ac0cc0486d3dd8c13355
jaoleal <jgleal@protonmail.com>
2025-08-14 12:22:08 -0300

    Refactor clean_data to include dev_bin and to be used from other scripts

--- contrib/clean_data.sh
+++ contrib/clean_data.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-# Find all 'tmp-db' directories in subdirectories
-dirs=$(find . -type d -name 'tmp-db')
+###############################################################################
+# Clean utility script
+# ---------------------
+# This script deletes:
+#   1. ./tmp directory in the project root
+#   2. All "tmp-db" directories found anywhere in the repository
+#   3. The ./contrib/dev_bin/ directory (where local cargo binaries are installed)
+#
+# It will:
+#   - Show the user which directories will be deleted
+#   - Ask for confirmation before proceeding
+#   - Perform deletions safely
+###############################################################################
 
-# Display the directories that will be deleted
-echo "The following 'tmp-db' directories will be deleted:"
-echo "$dirs"
+clean() {
+    tmp_dir="./tmp"
+    contrib_bin_dir="./contrib/dev_bin"
+    tmp_db_dirs=$(find . -type d -name 'tmp-db')
 
-# Prompt the user for confirmation
-read -r -p "Are you sure you want to delete './tmp' and all 'tmp-db' directories listed above? [y/N] " ans
+    echo "The following directories will be deleted:"
+    echo "$tmp_dir"
+    echo "$contrib_bin_dir"
+    echo "$tmp_db_dirs"
 
-# Check the user's response
-if [[ "$ans" =~ ^[Yy]$ ]]; then
-    # User confirmed, proceed with deletion
+    read -r -p "Are you sure you want to delete the above directories? [y/N] " ans
 
-    # Delete 'tmp' in the current directory (if run via justfile this is the root)
-    rm -rf tmp
+    if [[ "$ans" =~ ^[Yy]$ ]]; then
+        echo "Proceeding with deletion..."
 
-    # Delete all 'tmp-db' directories found
-    find . -type d -name 'tmp-db' -exec rm -rf {} +
+        rm -rf "$tmp_dir"
 
-    echo "Directories deleted."
-else
-    # User did not confirm, cancel the operation
-    echo "Deletion cancelled."
-fi
+        rm -rf "$contrib_bin_dir"
+
+        find . -type d -name 'tmp-db' -exec rm -rf {} +
+
+        echo "Directories deleted."
+    else
+        echo "Deletion cancelled."
+    fi
+}
+
+clean
