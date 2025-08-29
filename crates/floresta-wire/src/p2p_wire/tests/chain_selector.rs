b69882d9e42eb2b06fea8bd369fe29b4a1906bf7
Luca DG <lucad70@pm.me>
2025-08-29 15:37:23 -0300

    Add check on whole acc
    Previous this commit, the accumulator was being partially checked.
    It had a check on only one of the leaves. Now, using acc_hashes! macro
    it builds a whole acc from the given strings, thus enabling correctly
    checking the accumulator.
    
    Also removed unused code, moved imports to module level and renamed
    headers_iter to invalid_filters_iter.
    
    Replaced value of number of blocks for cnst. Previously it used a
    hardcoded value of 119, which was different then the available 120
    blocks.
    
    Replaced assert to consider all the blocks available. Previously it was
    cutting short of one block (119 instead of 120).
    
    Removed outdated comment.

--- crates/floresta-wire/src/p2p_wire/tests/chain_selector.rs
+++ crates/floresta-wire/src/p2p_wire/tests/chain_selector.rs
@@ -45,7 +45,7 @@ mod tests_utils {
 
         let mut headers = get_test_headers();
         headers.remove(0);
-        headers.truncate(119);
+        headers.truncate(NUM_BLOCKS);
         for header in headers {
             chain.accept_header(header).unwrap();
         }
@@ -73,7 +73,6 @@ mod tests_utils {
                 receiver,
                 i as u32,
             );
-            let _peer = peer.clone();
 
             node.peers.insert(i as u32, peer);
         }
@@ -90,10 +89,14 @@ mod tests_utils {
 mod tests {
     use std::str::FromStr;
 
+    use bitcoin::BlockHash;
     use bitcoin::Network;
     use floresta_chain::pruned_utreexo::BlockchainInterface;
     use floresta_chain::pruned_utreexo::UpdatableChainstate;
+    use floresta_common::acchashes;
+    use floresta_common::prelude::HashMap;
     use rustreexo::accumulator::node_hash::BitcoinNodeHash;
+    use rustreexo::accumulator::stump::Stump;
 
     use crate::p2p_wire::tests::chain_selector::tests_utils::setup_node;
     use crate::p2p_wire::tests::chain_selector::tests_utils::NUM_BLOCKS;
@@ -111,13 +114,13 @@ mod tests {
 
         let mut false_filters = true_filters.clone();
 
-        let headers_iter = headers
+        let invalid_filters_iter = headers
             .iter()
             .enumerate()
             .take(NUM_BLOCKS)
             .skip(STARTING_LIE_BLOCK_HEIGHT);
 
-        for (i, _) in headers_iter {
+        for (i, _) in invalid_filters_iter {
             false_filters.remove(&headers[i].block_hash());
             false_filters.insert(headers[i].block_hash(), create_false_acc(i));
         }
@@ -129,24 +132,25 @@ mod tests {
 
         let chain = setup_node(peers, true, Network::Signet).await;
         let best_block = chain.get_best_block().unwrap();
-        assert_eq!(best_block.1, headers[119].block_hash());
+        assert_eq!(best_block.1, headers[NUM_BLOCKS].block_hash());
 
-        let root_hashes = chain.get_root_hashes();
-        // the following assert is checking the third root of the accumulator at height 119
-        assert_eq!(
-            root_hashes[3],
-            BitcoinNodeHash::from_str(
-                "bfe030a7a994b921fb2329ff085bd0f2351cb5fa251985d6646aaf57954b782b"
-            )
-            .unwrap()
-        );
-        assert_eq!(root_hashes.len(), 6);
+        let acc_received = chain.acc();
+
+        let acc = Stump {
+            leaves: 120,
+            roots: acchashes![
+                "fbbff1a533f80135a0cb222859297792d5c9d1cec801a2793ac15184905e672c",
+                "42554b3aab845bf18397188fc21f1f39cfc742f36bdb1aae70dd60a39c1fd9b9",
+                "2782a7bd0f93d57efb8611c90d41a94d520bceded1fc6c0050b4133db24a15d0",
+                "d86dbb6f4c3c258e6a83ae0f349cbee695b10b2b677a02f12e5aefac04d368c9"
+            ]
+            .to_vec(),
+        };
+        assert_eq!(acc, acc_received);
     }
 
     #[tokio::test]
     async fn ten_peers_one_honest() {
-        use bitcoin::BlockHash;
-        use floresta_common::prelude::HashMap;
         let essentials = get_essentials();
         let headers = essentials.headers.to_vec();
         let blocks = essentials.blocks;
@@ -176,18 +180,20 @@ mod tests {
 
         let chain = setup_node(peers, true, Network::Signet).await;
         let best_block = chain.get_best_block().unwrap();
-        assert_eq!(best_block.1, headers[119].block_hash());
+        assert_eq!(best_block.1, headers[NUM_BLOCKS].block_hash());
 
-        let root_hashes = chain.get_root_hashes();
+        let acc_received = chain.acc();
 
-        // the following assert is checking the third root of the accumulator at height 119
-        assert_eq!(
-            root_hashes[3],
-            BitcoinNodeHash::from_str(
-                "bfe030a7a994b921fb2329ff085bd0f2351cb5fa251985d6646aaf57954b782b"
-            )
-            .unwrap()
-        );
-        assert_eq!(root_hashes.len(), 6);
+        let acc = Stump {
+            leaves: 120,
+            roots: acchashes![
+                "fbbff1a533f80135a0cb222859297792d5c9d1cec801a2793ac15184905e672c",
+                "42554b3aab845bf18397188fc21f1f39cfc742f36bdb1aae70dd60a39c1fd9b9",
+                "2782a7bd0f93d57efb8611c90d41a94d520bceded1fc6c0050b4133db24a15d0",
+                "d86dbb6f4c3c258e6a83ae0f349cbee695b10b2b677a02f12e5aefac04d368c9"
+            ]
+            .to_vec(),
+        };
+        assert_eq!(acc, acc_received);
     }
 }
