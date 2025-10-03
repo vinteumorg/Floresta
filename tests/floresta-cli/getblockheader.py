a7b2b04a9cef28ccd31b2a9c5699bde944620982
qlrddev <qlrddev@gmail.com>
2025-10-03 16:30:20 -0300

    test: apply tests to the fixed `getblockheader` rpc
    This commit add a cycle of connections and mining to the
    `getblockheader` test so it can check the header for 10 different
    heights.

--- tests/floresta-cli/getblockheader.py
+++ tests/floresta-cli/getblockheader.py
@@ -4,68 +4,159 @@ floresta_cli_getblockheader.py
 This functional test cli utility to interact with a Floresta node with `getblockheader`
 """
 
+import re
+import time
+import math
 from test_framework import FlorestaTestFramework
-from test_framework.rpc.floresta import REGTEST_RPC_SERVER
+
+DATA_DIR = FlorestaTestFramework.get_integration_test_dir()
+BLOCKS = 101
 
 
-class GetBlockheaderHeightZeroTest(FlorestaTestFramework):
+class GetBlockheaderTest(FlorestaTestFramework):
     """
-    Test `getblockheader` with a fresh node and expect a result like this:
-
-    ````bash
-    $> ./target/release floresta_cli --network=regtest getblockheader \
-        0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206
-    {
-       "version": 1,
-       "prev_blockhash": "0000000000000000000000000000000000000000000000000000000000000000",
-       "merkle_root": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
-       "time": 1296688602,
-       "bits": 545259519,
-       "nonce": 2
-    }
-    ```
+    Test florestad's `getbestblockheader` by running three nodes in
+    a "semi-triangle" network structure, where florestad and bitcoind
+    nodes are connected to utreexod, but not connected between them.
+    Then assert that the same get_blockheader in three nodes, before mining
+    and after mining.
     """
-
-    nodes = [-1]
-    version = 1
-    blockhash = "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"
-    prev_blockhash = "0000000000000000000000000000000000000000000000000000000000000000"
-    merkle_root = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
-    time = 1296688602
-    bits = 545259519
-    nonce = 2
 
     def set_test_params(self):
         """
-        Setup a single node
+        Setup a florestad/bitcoind peers and a utreexod mining node
         """
-        self.florestad = self.add_node(variant="florestad")
+        name = self.__class__.__name__.lower()
+        self.v2transport = False
+        self.data_dirs = GetBlockheaderTest.create_data_dirs(DATA_DIR, name, 3)
+        self.florestad = self.add_node(
+            variant="florestad", extra_args=[f"--data-dir={self.data_dirs[0]}"]
+        )
+
+        self.utreexod = self.add_node(
+            variant="utreexod",
+            extra_args=[
+                f"--datadir={self.data_dirs[1]}",
+                "--miningaddr=bcrt1q4gfcga7jfjmm02zpvrh4ttc5k7lmnq2re52z2y",
+                "--prune=0",
+            ],
+        )
+
+        self.bitcoind = self.add_node(
+            variant="bitcoind", extra_args=[f"-datadir={self.data_dirs[2]}"]
+        )
 
     def run_test(self):
         """
-        Run JSONRPC and get the header of the genesis block
+        Run a florestad/bitcoind/utreexod nodes. Then mine some blocks
+        with utreexod. After that, connect the nodes and wait for them to sync.
+        Finally, test the `getblockheader` rpc command checking if it's
+        different from genesis one and equals to utreexod one.
         """
-        # Start node
         self.run_node(self.florestad)
+        self.run_node(self.utreexod)
+        self.run_node(self.bitcoind)
 
-        # Test assertions
-        response = self.florestad.rpc.get_blockheader(
-            GetBlockheaderHeightZeroTest.blockhash
+        self.log("=== Mining  with utreexod")
+        self.utreexod.rpc.generate(BLOCKS)
+        time.sleep(5)
+
+        self.log("=== Connect floresta to utreexod")
+        host = self.utreexod.get_host()
+        port = self.utreexod.get_port("p2p")
+        self.florestad.rpc.addnode(
+            f"{host}:{port}", command="onetry", v2transport=False
         )
-        self.assertEqual(response["version"], GetBlockheaderHeightZeroTest.version)
-        self.assertEqual(
-            response["prev_blockhash"], GetBlockheaderHeightZeroTest.prev_blockhash
+
+        self.log("=== Waiting for floresta to connect to utreexod...")
+        time.sleep(5)
+        peer_info = self.florestad.rpc.get_peerinfo()
+        self.assertMatch(
+            peer_info[0]["user_agent"],
+            re.compile(r"/btcwire:\d+\.\d+\.\d+/utreexod:\d+\.\d+\.\d+/"),
         )
-        self.assertEqual(
-            response["merkle_root"], GetBlockheaderHeightZeroTest.merkle_root
+
+        self.log("=== Connect bitcoind to utreexod")
+        host = self.utreexod.get_host()
+        port = self.utreexod.get_port("p2p")
+        self.bitcoind.rpc.addnode(f"{host}:{port}", command="onetry", v2transport=False)
+
+        self.log("=== Waiting for bitcoind to connect to utreexod...")
+        time.sleep(5)
+        peer_info = self.bitcoind.rpc.get_peerinfo()
+        self.assertMatch(
+            peer_info[0]["subver"],
+            re.compile(r"/btcwire:\d+\.\d+\.\d+/utreexod:\d+\.\d+\.\d+/"),
         )
-        self.assertEqual(response["time"], GetBlockheaderHeightZeroTest.time)
-        self.assertEqual(response["bits"], GetBlockheaderHeightZeroTest.bits)
-        self.assertEqual(response["nonce"], GetBlockheaderHeightZeroTest.nonce)
+
+        self.log("=== Wait for the nodes to sync...")
+        time.sleep(5)
+
+        for height in range(BLOCKS):
+            self.log(
+                f"=== Check floresta have the same blockheader as core for height {height}..."
+            )
+            floresta_hash = self.florestad.rpc.get_blockhash(height)
+            bitcoin_hash = self.bitcoind.rpc.get_blockhash(height)
+            self.assertEqual(floresta_hash, bitcoin_hash)
+
+            floresta_header = self.florestad.rpc.get_blockheader(floresta_hash, False)
+            bitcoin_header = self.bitcoind.rpc.get_blockheader(bitcoin_hash, False)
+            self.assertEqual(floresta_header, bitcoin_header)
+
+            floresta_verbose_header = self.florestad.rpc.get_blockheader(
+                floresta_hash, True
+            )
+            bitcoin_verbose_header = self.bitcoind.rpc.get_blockheader(
+                bitcoin_hash, True
+            )
+
+            # Not test in regtest network the "difficulty" field since
+            # rust-bitcoin apply correctly while core have a bug in this field
+            for key in (
+                "hash",
+                "confirmations",
+                "height",
+                "version",
+                "versionHex",
+                "merkleroot",
+                "time",
+                "mediantime",
+                "nonce",
+                "bits",
+                "target",
+                "chainwork",
+                "nTx",
+            ):
+                self.assertEqual(
+                    floresta_verbose_header[key], bitcoin_verbose_header[key]
+                )
+
+            # These assertions will run only in some
+            # general conditions like the height
+            if height != 0:
+                self.assertEqual(
+                    floresta_verbose_header["previousblockhash"],
+                    bitcoin_verbose_header["previousblockhash"],
+                )
+
+                if height > 1:
+                    self.assertAlmostEqual(
+                        floresta_verbose_header["difficulty"],
+                        bitcoin_verbose_header["difficulty"],
+                    )
+
+                if height != 10:
+                    self.assertEqual(
+                        floresta_verbose_header["nextblockhash"],
+                        bitcoin_verbose_header["nextblockhash"],
+                    )
+
+            time.sleep(0.1)
 
         # stop the node
         self.stop()
 
 
 if __name__ == "__main__":
-    GetBlockheaderHeightZeroTest().main()
+    GetBlockheaderTest().main()
