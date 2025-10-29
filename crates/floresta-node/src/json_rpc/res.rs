34e696777363c5b2ca44518babf221be081fe18f
moisesPomilio <93723302+moisesPompilio@users.noreply.github.com>
2025-10-29 09:44:58 -0300

    fix(rpc): chain work display and implement proper chain work calculation with JSON-RPC error handling

--- crates/floresta-node/src/json_rpc/res.rs
+++ crates/floresta-node/src/json_rpc/res.rs
@@ -310,6 +310,9 @@ pub enum JsonRpcError {
 
     /// Raised if when the rescanblockchain command, with the timestamp flag activated, contains some timestamp thats less than the genesis one and not zero which is the default value for this arg.
     InvalidTimestamp,
+
+    // Error related to Chain Work calculations
+    ChainWork(String),
 }
 
 impl Display for JsonRpcError {
@@ -339,6 +342,7 @@ impl Display for JsonRpcError {
             JsonRpcError::Wallet(e) => write!(f, "Wallet error: {e}"),
             JsonRpcError::Filters(e) => write!(f, "Error with filters: {e}"),
             JsonRpcError::InvalidAddnodeCommand => write!(f, "Invalid addnode command"),
+            JsonRpcError::ChainWork(e) => write!(f, "Chain work error: {e}"),
         }
     }
 }
